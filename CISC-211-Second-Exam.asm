fd0b7f35b405e5b06c87980b4f34c83107a3ddaf
josorio-io <146808534+josorio-io@users.noreply.github.com>
2024-07-08 18:04:56 -0700

    added comments to prove understanding

--- CISC-211-Second-Exam.asm
+++ CISC-211-Second-Exam.asm
@@ -1,8 +1,12 @@
+; initialize variables a, b, c, d as integers [ double words ]
+
 section .data
     a dd 1
     b dd 2
     c dd 3
     d dd 4
+
+; un-initialized variable x to store result
 
 section .bss
     x resd 1
@@ -12,17 +16,17 @@ section .text
 
 _start:
 
-    mov eax, [a]
-    mov ebx, [b]
-    imul eax, ebx
+    mov eax, [a]    ; move a's value to eax register
+    mov ebx, [b]    ; move b's value to ebx register
+    imul eax, ebx   ; multiply eax by ebx [ store in eax register ]
 
-    mov ecx, [c]
-    mov edx, [d]
-    imul ecx, edx
+    mov ecx, [c]    ; move c's value to ecx register
+    mov edx, [d]    ; move d's value to edx register
+    imul ecx, edx   ; multiply ecx by edx [ store in ecx register ]
 
-    add eax, ecx
+    add eax, ecx    ; add eax by ecx [ store in eax register ]
 
-    mov [x], eax
+    mov [x], eax    ; move eax to memory address of x
 
     mov eax, 1
-    int 0x80
+    int 0x80        ; exit program
