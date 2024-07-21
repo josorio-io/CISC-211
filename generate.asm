a505a2f648636b9ef7d47241b0cc8849abe000ba
josorio-io <146808534+josorio-io@users.noreply.github.com>
2024-07-20 19:29:47 -0700

    blob

--- generate.asm
+++ generate.asm
@@ -1,3 +1,14 @@
+; move 0 to ECX register to begin count
+; move 2 to ECX register per iteration
+; compare ECX with 20 per iteration
+; if ECX equals 20, loop again
+; else exit program
+
+; biggest issue I had was with ending the loop
+; I allowed the loop to run one more time to end the loop
+; so the final value of ECX is 22, with the intent to exit out of the loop
+
+
 section .text
         global _start
 
