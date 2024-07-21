827238266db5c242f12c6e3d3afd323f0e4d3cd0
josorio-io <146808534+josorio-io@users.noreply.github.com>
2024-07-20 19:32:24 -0700

    blob

--- compare.asm
+++ compare.asm
@@ -1,3 +1,12 @@
+; my approach to this assignment was starting with one number
+; and moving to the next while storing in the EAX register the
+; largest number so far encountered until the end of the 
+; psuedo-array
+
+; if any number is larger than that in the EAX register,
+; the EAX register is the updated until there are no numbers
+; to compare
+
 section .data
     num1 dd 1
     num2 dd 2
