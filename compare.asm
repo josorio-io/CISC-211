f8bb69cef1960fcea66b4fe829bd1b912bbff87f
josorio-io <146808534+josorio-io@users.noreply.github.com>
2024-07-20 19:19:05 -0700

    compare.asm

---
+++ compare.asm
@@ -0,0 +1,44 @@
+section .data
+    num1 dd 1
+    num2 dd 2
+    num3 dd 3
+    num4 dd 4
+    num5 dd 5
+
+section .bss
+    largest resd 1
+
+section .text
+    global _start
+
+_start:
+    mov eax, [num1]
+    mov ebx, [num2]
+    cmp eax, ebx
+    jge next1
+    mov eax, ebx
+
+next1:
+    mov ebx, [num3]
+    cmp eax, ebx
+    jge next2
+    mov eax, ebx
+
+next2:
+    mov ebx, [num4]
+    cmp eax, ebx
+    jge next3
+    mov eax, ebx
+
+next3:
+    mov ebx, [num5]
+    cmp eax, ebx
+    jge finish
+    mov eax, ebx
+
+finish:
+    mov [largest], eax
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
